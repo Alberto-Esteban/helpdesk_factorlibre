c4b2125b62531cfefaed3ffc8b6acd01803bfc67
Alberto Esteban <62519994+Alberto-Esteban@users.noreply.github.com>
2020-07-28 16:45:18 +0200

    ADD helpdesk_project

---
+++ helpdesk_project/tests/test_helpdesk_project.py
@@ -0,0 +1,8 @@
+'''
+def ticket_in_proyect(self):
+    #ticket belongs to a project
+    -we create the new ticket
+    -assign the new ticket to a project
+    -button ticket count +1
+    -check the count button is right
+'''
